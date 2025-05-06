d96b651db4999a4fc950544379566d88e0d19f60
xentriom <c04jason@gmail.com>
2025-05-06 13:13:23 -0400

    update and sidebar

---
+++ app/template.tsx
@@ -0,0 +1,18 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export default function Template({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  )
+}
